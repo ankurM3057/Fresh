da048ae8ec93b9bcff2a260aff6a320797990587
ShreyaKulhalli <48976678+ShreyaKulhalli@users.noreply.github.com>
2019-04-04 14:15:30 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,6 +2,7 @@
 int main()
 {
 printf("hello world");
-printf("\nThis commit was made by Rishabh Agarwal") // commit
+printf("\nThis commit was made by Rishabh Agarwal") ;// commit
+printf("This commit was made by Shreya Kulhalli\n");
 return 0;
 }
